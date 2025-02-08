e0668ac75536cc42dcda0849529f7a27a7698b30
Waterlemon14 <edaldemita@up.edu.ph>
2025-02-08 14:49:17 +0800

    fix: added previously worked on pages from facilitySide folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,7 @@
 </script>
 
 {@render children()}
+
+<!-- add repeating elements for all  pages here -->
+
+<!-- like when there is a navigation bar for all pages, you can add here then include <slot /> to render rest of page-->
