48d83077ac7341f776d950ac2a8a632c48d9e3ca
Waterlemon14 <edaldemita@up.edu.ph>
2025-02-11 08:09:58 +0800

    added temp data testing

--- src/routes/facility/SignInBox.svelte
+++ src/routes/facility/SignInBox.svelte
@@ -1,11 +1,15 @@
 <script lang='ts'>
 	let { fid=$bindable(), password=$bindable() } = $props();
 	let show = $state(true);
-	</script>
+
+	import { goto } from '$app/navigation';
+
+</script>
+
+
 
 <!-- @elle, @paul: padesign -->
 <form 
-	use:focusTrap={true} 
 	class="bg-gray-400 m-6 space-y-2 rounded-2xl p-6 "
 >
 
@@ -16,6 +20,7 @@
 		<label class="label">
 			<input 
 				bind:value={fid}
+				name="Facility ID"
 				class="input" 
 				type="text" 
 				placeholder="ID" 
@@ -29,6 +34,7 @@
 
 			<input 
 				bind:value={password}
+				name="Password"
 				class="input" 
 				type={show ? 'text' : 'password'}
 				placeholder="Password"
@@ -52,7 +58,7 @@
 	<!-- to change to function check -->
 	<button 
 		class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800" 
-		href="/facility/{fid}/dashboard"
+		onclick="{ () => goto('./facility/' + fid + '/dashboard') }"
 	>
 		Sign In
 	</button>
--- src/routes/patient/+page.svelte
+++ src/routes/patient/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Search from '$lib/Search.svelte'
   import Header from '$lib/Header.svelte'
+  import info from './info.json'
 </script>
 
 <Header />
@@ -12,6 +13,8 @@
   <button type="button" class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-900">Sign In</button>
 
   <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Sign Up</button>
+
+  <p>{info.admins[0].userID}</p>
 
 </div>
 
