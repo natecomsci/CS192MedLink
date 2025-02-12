a469fdc387ed386499d0b5bd3bee359e7c477f53
Waterlemon14 <edaldemita@up.edu.ph>
2025-02-12 13:13:21 +0800

    fix: removed server access to db for now

--- src/routes/facility/+page.server.ts
+++ src/routes/facility/+page.server.ts
@@ -1,10 +1,10 @@
 import { prisma } from '$lib/prisma';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-  const response = await prisma.user.findMany({
-      where: { userId: 1 },
-    })
+// export const load = (async () => {
+//   const response = await prisma.user.findMany({
+//       where: { userId: 1 },
+//     })
 
-  return { feed: response };
-}) satisfies PageServerLoad;
+//   return { feed: response };
+// }) satisfies PageServerLoad;
