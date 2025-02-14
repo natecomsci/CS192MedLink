3ef17fb30d900582a3c4b30fb9b039e3b01611e3
Waterlemon14 <edaldemita@up.edu.ph>
2025-02-14 11:42:12 +0800

    feat: initial login server side functions

--- src/routes/facility/+page.server.ts
+++ src/routes/facility/+page.server.ts
@@ -1,7 +1,25 @@
+import { fail } from '@sveltejs/kit';
+// import * as db from '$lib/server/db';
 import type { PageServerLoad, Actions } from './$types';
 
 export const actions = {
-  default: async (event) => {
-    // TODO log the user in
+  default: async ({ cookies, request }) => {
+    const data = await request.formData();
+    const email = data.get('email');
+    const password = data.get('password');
+
+    if (!email) {
+      return fail(400, { email, missing: true });
+    }
+
+    // const user = await db.getUser(email);
+
+    // if (!user || user.password !== password) {
+    //   return fail(400, { email, incorrect: true });
+    // }
+
+    // cookies.set('sessionid', await db.createSession(user), { path: '/facility/' + user.id + '/dashboard' });
+
+    return { success: true };
   }
 } satisfies Actions;
