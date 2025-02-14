9f81ac7cdd60fc5015c9b9bbd4ba6f8c6e6343db
Waterlemon14 <edaldemita@up.edu.ph>
2025-02-14 09:42:23 +0800

    refactor: added debug header for ease of routing

---
+++ src/routes/DebugRouting.svelte
@@ -0,0 +1,34 @@
+<header class="sticky top-0 z-10 w-full bg-red-500 shadow-md">
+    <nav>
+        <ul class="flex justify-center">
+            <li>
+                <a class="block p-4" href="/">P-Guest</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/patient/1">P-Reg</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/patient/1/settings">P-Set</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/patient/1/bookmarks">P-Book</a>
+            </li>
+
+            <li>
+                <a class="block p-4" href="/facility/">F-Land</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/facility/1/dashboard">F-Dash</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/facility/1/dashboard/addService">F-Add</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/facility/1/dashboard/settings">F-Set</a>
+            </li>
+            <li>
+                <a class="block p-4" href="/facility/1/dashboard/settings/editGeneralInfo">F-Edit</a>
+            </li>
+        </ul>
+    </nav>
+</header>
