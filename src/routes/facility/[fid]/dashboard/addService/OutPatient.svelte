7fd5978e3b9923d82e20760656feb644372a2d72
Waterlemon14 <edaldemita@up.edu.ph>
2025-02-17 09:08:31 +0800

    fix: phone format check

--- src/routes/facility/[fid]/dashboard/addService/OutPatient.svelte
+++ src/routes/facility/[fid]/dashboard/addService/OutPatient.svelte
@@ -5,15 +5,17 @@
 <h3>{serviceType}</h3>
 
 <label>
-  Base Price
+  <h3>Service Phone Number</h3>
+  <p>
+    Format: +63 912 345 6789
+  </p>
   <input 
-    name="price" 
-    type="number"
-    placeholder=100
-    class = "w-20"
-    required
+    name="phoneNumber"
+    type="tel"
+    value="+63 9"
+    pattern="+63 9[0-9]{2} [0-9]{3} [0-9]{4}"
+    required 
   >
-  pesos
 </label>
 
 <label>
