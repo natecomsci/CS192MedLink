bbbbea1ced079dedc6ea632e734c3a2eecb3f91f
amabyado <amabyado@up.edu.ph>
2025-02-26 17:21:15 +0800

    minor: changed hardcoded photo link sa update general information to the temporary placeholder we have

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -620,7 +620,7 @@ export class FacilityDAO {
           },
           data: {
             name              : data.name,
-            photo             : data.photo ?? "",
+            photo             : data.photo ?? "https://placehold.co/1920x1440/png",
             email             : data.email,
             phoneNumber       : data.phoneNumber,
             facilityType      : data.facilityType,
