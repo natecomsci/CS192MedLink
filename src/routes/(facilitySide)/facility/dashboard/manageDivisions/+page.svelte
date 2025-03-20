7ba28266a35d8890f836ce2b0366d09b7b505dbd
ElleDiablo <100169794+ElleDiablo@users.noreply.github.com>
2025-03-20 20:48:55 +0800

    create division layout

--- src/routes/(facilitySide)/facility/dashboard/manageDivisions/+page.svelte
+++ src/routes/(facilitySide)/facility/dashboard/manageDivisions/+page.svelte
@@ -123,7 +123,7 @@
 
     <!-- Scrollable List Container -->
     <div class="space-y-3 mt-4 w-2/3 border  h-[calc(100vh-300px)] overflow-y-auto pr-8 pt-5">
-      {#each admins as  { fname, mname, lname, adminID, divisionID }}
+        {#each Array(20) as _,j}
         <div class="flex items-center justify-between p-3 bg-white rounded-[30px] shadow-[0px_4px_10px_rgba(0,0,0,0.3)] w-full">
           <div>
             <h3 class="text-lg font-bold text-gray-900 px-4">Division Name</h3>
