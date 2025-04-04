9d707efb42efcdf2efae2610a011f832af270336
Waterlemon14 <edaldemita@up.edu.ph>
2025-04-04 18:04:41 +0800

    landing page buttons

--- src/routes/(patientSide)/+page.svelte
+++ src/routes/(patientSide)/+page.svelte
@@ -42,6 +42,37 @@
       <p class="text-red-600 mt-2">{form.error}</p>
     {/if}
 
+    <div class="grid grid-cols-2 p-10">
+      <div class="p-5">
+        <a href="/search/Ambulance"
+          class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-500 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        >
+          Ambulance
+        </a>
+      </div>
+      <div class="p-5">
+      <a href="/search/Blood%20Bank"
+        class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-500 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      >
+        Blood Bank
+      </a>
+      </div>
+      <div class="p-5">
+      <a href="/search/Intensive%20Care%20Unit"
+        class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-500 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      >
+        Intensive Care Unit
+      </a>
+      </div>
+      <div class="p-5">
+      <a href="/search/Emergency%20Room"
+        class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-500 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      >
+        Emergency Room
+      </a>
+      </div>
+    </div>
+
     {#if showFilter}
       <p>filters here</p>
     {/if}
