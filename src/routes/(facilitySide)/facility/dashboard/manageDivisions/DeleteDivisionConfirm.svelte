5bf66afaf72bccbb938d424454c578a08b1f17f1
Waterlemon14 <edaldemita@up.edu.ph>
2025-04-24 17:11:11 +0800

    delete division reload list

--- src/routes/(facilitySide)/facility/dashboard/manageDivisions/DeleteDivisionConfirm.svelte
+++ src/routes/(facilitySide)/facility/dashboard/manageDivisions/DeleteDivisionConfirm.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
   import type { AdminDTO, DivisionDTO, ServiceDTO } from "$lib";
-  import type { ActionData } from "./$types";
+    import { pagingQueryHandler } from "$lib/postHandlers";
+  import type { ActionData, PageData } from "./$types";
 
-  let { form, divisionID, currPopUp = $bindable() }: { form: ActionData, divisionID: String, currPopUp: String } = $props();
+  let { data, 
+        form, 
+        currPopUp = $bindable(),
+        divisionID,
+        divisions = $bindable(),
+        currentPage = $bindable(),
+        totalPages = $bindable(),
+        perPage
+        }:{ data:PageData, 
+            form: ActionData, 
+            currPopUp: String, 
+            divisionID: String, 
+            divisions:DivisionDTO[], 
+            currentPage: number, 
+            totalPages: number, 
+            perPage: number
+          } = $props();
 
   let admins: AdminDTO[] = $state([])
   let facilityDivisions: DivisionDTO[] = $state([])
@@ -65,6 +82,26 @@
     }
   }
 
+  async function getNewDivisions() {
+    try {
+      const rv = await pagingQueryHandler({
+        page: 'divisions',
+        query: '',
+        isInQueryMode:false,
+        currentPage:1,
+        change:0,
+        totalPages:1,
+        perPage,
+        viewedDivisionID: "Default"
+      });
+      divisions =  rv.list
+      currentPage = 1
+      totalPages = rv.totalPages
+    } catch (error) {
+      console.log((error as Error).message)
+    }
+  }
+
 </script>
 <form
   method="POST"
@@ -75,6 +112,7 @@
       await update({invalidateAll:true});
       if (form?.success) {
           currPopUp = ''
+          getNewDivisions()
       }
     };
   }}
