aaf0bfb5468a7ea101bbcfda915ceab31bcec52e
Waterlemon14 <aeisenhowerii@gmail.com>
2025-07-19 17:14:33 +0800

    prisma redefinition

--- src/lib/server/dataLayer/prisma.ts
+++ src/lib/server/dataLayer/prisma.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") {
-  global.prisma = prisma;
+declare global {
+  var prisma: PrismaClient;
 }
 
-export { prisma }
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+export default prisma;
